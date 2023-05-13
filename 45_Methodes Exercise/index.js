f2b79f27010cd33e1bf5073368bfc4cbec638f81
Uginesque <133516654+Uginesque@users.noreply.github.com>
2023-05-13 23:41:08 +0900

    Update index.js
    Try와 catch 문제가 작성되어 있어서, 메소드 만들기 문제의 답안으로 수정했습니다.

--- 45_Methodes Exercise/index.js
+++ 45_Methodes Exercise/index.js
@@ -1,7 +1,10 @@
-try {
-    hello.toUpperCase();
-} catch {
-    console.log("ERROR"); // 에러 문구는 뜨지 않고 콘솔에 ERROR 문자열이 출력됨
-}
+const square = {
+    area(side) {
+        return side * side;
+    },
+    perimeter(side) {
+        return side * 4;
+    }
+};
 
-console.log("그래서 멈추지 않고 다음 줄을 실행할 수 있습니다.")
+// SHORTHAND식 메소드 표기법
